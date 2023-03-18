2431c3e81eb057b1d69f00e03b0be5e241e9b581
finall00 <vvcruzpelissoni08@gmail.com>
2023-03-18 14:00:15 -0300

    first commit

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,11 @@
+const connection = require('./connection');
+
+const getAll = async() => {
+ const tasks = await connection.execute('SELECT * FROM tasks');
+ return tasks;
+};
+
+
+module.exports = {
+    getAll
+};
