01fe382e9173cb5c2c35de3dfba4a8f84a7bf8c1
finall00 <vvcruzpelissoni08@gmail.com>
2023-03-19 12:45:43 -0300

    adicionando o banco e modificações gerais

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,7 +2,15 @@ const connection = require('./connection');
 
 const getAll = async() => {
  const tasks = await connection.execute('SELECT * FROM tasks');
+ return tasks[0];
+ //retorna o array com os dados, pega so o primeiro array 
+/*
+ tmb da pra fazer usando array destruction
+ 
+ const [tasks] = await connection.execute('SELECT * FROM tasks');
  return tasks;
+ */
+
 };
 
 
