715114bddc876f325f60e9ca6503fd9b5ee61ab7
finall00 <vvcruzpelissoni08@gmail.com>
2023-03-22 15:06:05 -0300

    frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = require('./router');
+const cors =  require('cors');
 
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
+
 
 module.exports = app;
